3e3e957515980c6b04013b07c9e3d3343bf22178
Crowdbotics <team@crowdbotics.com>
2019-06-21 03:39:33 +0000

    initial commit

---
+++ android/app/src/main/java/com/paxafe_4963/MainActivity.java
@@ -0,0 +1,15 @@
+package com.paxafe_4963;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "paxafe_4963";
+    }
+}
